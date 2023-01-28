92b889d55d6a404cdecfba49b74811f0772ebeb4
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-01-28 22:25:09 +0530

    Time: 20 ms (49.27%), Space: 13.6 MB (43.31%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def isSameTree(self, p, q):
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return p == q
