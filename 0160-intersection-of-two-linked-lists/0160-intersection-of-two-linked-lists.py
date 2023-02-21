ba8be78399ec4aa09bf5e276026dfee52db91d98
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-02-21 22:20:40 +0530

    Update 0160-intersection-of-two-linked-lists.py

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -2,13 +2,11 @@ class Solution(object):
     def getIntersectionNode(self, headA, headB):
         if headA is None or headB is None:
             return None
-
+        
         pa = headA 
         pb = headB
-
+        
         while pa is not pb:
             pa = headB if pa is None else pa.next
             pb = headA if pb is None else pb.next
-
         return pa
-        
