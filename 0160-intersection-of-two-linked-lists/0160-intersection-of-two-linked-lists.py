5ba4b81f462cb9333975e9957871bfea7939398b
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-02-21 22:20:10 +0530

    Update 0160-intersection-of-two-linked-lists.py

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -1,9 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution(object):
     def getIntersectionNode(self, headA, headB):
         if headA is None or headB is None:
