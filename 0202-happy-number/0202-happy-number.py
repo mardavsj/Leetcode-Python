3cd1336a964ba986f42dbe915bb3679ab849c1c5
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-06-29 08:12:53 +0530

    Time: 53 ms (42.23%), Space: 16.4 MB (31.18%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isHappy(self, n):
+        seen = set()
+        while True:
+            if n==1:
+                return True
+            elif n in seen:
+                return False
+            else:
+                seen.add(n)
+                n = sum(int(i)**2 for i in str(n))
