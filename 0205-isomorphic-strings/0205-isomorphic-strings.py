3942e847799989c1485599a468021340acb6597a
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-07-01 21:06:08 +0530

    Time: 53 ms (77.96%), Space: 17.1 MB (15.46%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        map1 = []
+        map2 = []
+        for idx in s:
+            map1.append(s.index(idx))
+        for idx in t:
+            map2.append(t.index(idx))
+        if map1 == map2:
+            return True
+        return False
