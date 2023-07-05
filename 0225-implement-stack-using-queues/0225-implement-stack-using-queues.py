623fb0ffcf664d6450ef4a83eef53b21a8e8f924
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-07-05 18:26:55 +0530

    Time: 43 ms (77.78%), Space: 16.4 MB (73.99%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -0,0 +1,18 @@
+class MyStack:
+
+    def __init__(self):
+        self.q = collections.deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+    def pop(self) -> int:
+        for i in range(len(self.q) - 1):
+            self.push(self.q.popleft())
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[-1]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
